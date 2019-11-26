329c7b27d499e0317221bb68920bc24fd75c4a3e
Flavio Ragni <47398415+flavioragni@users.noreply.github.com>
2019-11-26 10:47:29 +0100

    Run FSL's BET on subjects T1

---
+++ restingState_fMRIexp/rs_project_run_bet.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#Run bet rs_project
+for current_sub in 28 #01 02 03 05 06 07 08 09 10 11 12 13 14 15 16 17 18 20 21 22 23 24 25 27
+do
+	if [ ! -d "/mnt/raidVol2/flavio.ragni/resting_state_project/SUB"$current_sub"/" ]; then
+		mkdir "/mnt/raidVol2/flavio.ragni/resting_state_project/SUB"$current_sub  
+	fi
+	/usr/local/fsl/bin/fsl_sub -l /mnt/raidVol2/flavio.ragni/messages/ bet "/mnt/raidVol2/flavio.ragni/resting_state_project/raw_data/SUB"$current_sub"/SUB"$current_sub".nii.gz" "/mnt/raidVol2/flavio.ragni/resting_state_project/SUB"$current_sub"/SUB"$current_sub"_brain.nii.gz"
+
+done
