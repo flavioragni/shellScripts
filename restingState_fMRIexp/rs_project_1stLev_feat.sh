3da6883b0d210476f541a0360a202fc3ea6ecd28
Flavio Ragni <47398415+flavioragni@users.noreply.github.com>
2019-11-26 10:48:24 +0100

    Run 1st level feat FSL univariate

---
+++ restingState_fMRIexp/rs_project_1stLev_feat.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#FEAT rs_project
+for current_sub in 01 02 03 05 06 07 08 09 10 11 12 13 14 15 16 17 18 20 21 22 23 24 25 27
+do
+	for current_run in 01 02 03 04 05 06 07 08 09
+	do 
+		/usr/local/fsl/bin/fsl_sub -l /mnt/raidVol2/flavio.ragni/messages/ feat "/mnt/raidVol2/flavio.ragni/resting_state_project/fsf/SUB"$current_sub"_RUN"$current_run"_rs_0.001_clustercorr.fsf" 
+	done
+done
