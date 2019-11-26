d4bd92553aba654033628356f3da75b73112fb74
Flavio Ragni <47398415+flavioragni@users.noreply.github.com>
2019-11-26 10:48:38 +0100

    Update rs_project_run_bet.sh

--- restingState_fMRIexp/rs_project_run_bet.sh
+++ restingState_fMRIexp/rs_project_run_bet.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #Run bet rs_project
-for current_sub in 28 #01 02 03 05 06 07 08 09 10 11 12 13 14 15 16 17 18 20 21 22 23 24 25 27
+for current_sub in 01 02 03 05 06 07 08 09 10 11 12 13 14 15 16 17 18 20 21 22 23 24 25 27 28
 do
 	if [ ! -d "/mnt/raidVol2/flavio.ragni/resting_state_project/SUB"$current_sub"/" ]; then
 		mkdir "/mnt/raidVol2/flavio.ragni/resting_state_project/SUB"$current_sub  
