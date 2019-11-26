871b069c9b901aef42cb6e75ab0f05e5524edf75
Flavio Ragni <47398415+flavioragni@users.noreply.github.com>
2019-11-26 11:01:55 +0100

    Run FSL's parametric modulation (1st level)

--- restingState_fMRIexp/rs_project_parametricModulation_1stLev_feat.sh
+++ restingState_fMRIexp/rs_project_parametricModulation_1stLev_feat.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #FEAT parametric modulation rs_project
-for current_sub in 01 03 05 06 07 09 10 11 12 13 15 16 17 18 #01 03 05 06 07 09 10 11 12 13 14 15 16 17 18 
+for current_sub in 01 03 05 06 07 09 10 11 12 13 15 16 17 18
 do
 	for current_run in 01 02 03 04 05 06 07 08 09
 	do 
