eb96c09d064688b1a3ca7dcdbb830682572bcba5
Flavio Ragni <47398415+flavioragni@users.noreply.github.com>
2019-11-28 11:33:03 +0100

    Extract motion correction parameters for each subject

--- restingState_fMRIexp/rs_project_extract_mc.sh
+++ restingState_fMRIexp/rs_project_extract_mc.sh
@@ -4,9 +4,9 @@ do
 	    '/mnt/raidVol2/flavio.ragni/resting_state_project/SUB'$current_sub'/SUB'$current_sub'_RUN02_rs_0.001_clustercorr.feat/mc/prefiltered_func_data_mcf_abs_mean.rms'\
 	    '/mnt/raidVol2/flavio.ragni/resting_state_project/SUB'$current_sub'/SUB'$current_sub'_RUN03_rs_0.001_clustercorr.feat/mc/prefiltered_func_data_mcf_abs_mean.rms'\
 	    '/mnt/raidVol2/flavio.ragni/resting_state_project/SUB'$current_sub'/SUB'$current_sub'_RUN04_rs_0.001_clustercorr.feat/mc/prefiltered_func_data_mcf_abs_mean.rms'\
-      '/mnt/raidVol2/flavio.ragni/resting_state_project/SUB'$current_sub'/SUB'$current_sub'_RUN05_rs_0.001_clustercorr.feat/mc/prefiltered_func_data_mcf_abs_mean.rms'\
-      '/mnt/raidVol2/flavio.ragni/resting_state_project/SUB'$current_sub'/SUB'$current_sub'_RUN06_rs_0.001_clustercorr.feat/mc/prefiltered_func_data_mcf_abs_mean.rms'\
-      '/mnt/raidVol2/flavio.ragni/resting_state_project/SUB'$current_sub'/SUB'$current_sub'_RUN07_rs_0.001_clustercorr.feat/mc/prefiltered_func_data_mcf_abs_mean.rms'\
-      '/mnt/raidVol2/flavio.ragni/resting_state_project/SUB'$current_sub'/SUB'$current_sub'_RUN08_rs_0.001_clustercorr.feat/mc/prefiltered_func_data_mcf_abs_mean.rms'\
-      '/mnt/raidVol2/flavio.ragni/resting_state_project/SUB'$current_sub'/SUB'$current_sub'_RUN09_rs_0.001_clustercorr.feat/mc/prefiltered_func_data_mcf_abs_mean.rms' > '/mnt/raidVol2/flavio.ragni/resting_state_project/mc/SUB'$current_sub'_mc.txt'
+      	    '/mnt/raidVol2/flavio.ragni/resting_state_project/SUB'$current_sub'/SUB'$current_sub'_RUN05_rs_0.001_clustercorr.feat/mc/prefiltered_func_data_mcf_abs_mean.rms'\
+            '/mnt/raidVol2/flavio.ragni/resting_state_project/SUB'$current_sub'/SUB'$current_sub'_RUN06_rs_0.001_clustercorr.feat/mc/prefiltered_func_data_mcf_abs_mean.rms'\
+            '/mnt/raidVol2/flavio.ragni/resting_state_project/SUB'$current_sub'/SUB'$current_sub'_RUN07_rs_0.001_clustercorr.feat/mc/prefiltered_func_data_mcf_abs_mean.rms'\
+            '/mnt/raidVol2/flavio.ragni/resting_state_project/SUB'$current_sub'/SUB'$current_sub'_RUN08_rs_0.001_clustercorr.feat/mc/prefiltered_func_data_mcf_abs_mean.rms'\
+            '/mnt/raidVol2/flavio.ragni/resting_state_project/SUB'$current_sub'/SUB'$current_sub'_RUN09_rs_0.001_clustercorr.feat/mc/prefiltered_func_data_mcf_abs_mean.rms' > '/mnt/raidVol2/flavio.ragni/resting_state_project/mc/SUB'$current_sub'_mc.txt'
 done
